e9be7c168de434b3c8e7795f39cdf5ab710c2c95
Diego Bueno <diehobueno@gmail.com>
2025-09-08 18:51:27 -0500

    config prod

--- webpack.config.js
+++ webpack.config.js
@@ -2,46 +2,53 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { ModuleFederationPlugin } = require("webpack").container;
 const path = require("path");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    publicPath: "auto",
-  },
-  devServer: {
-    port: 3002,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
+  return {
+    mode: isProd ? 'production' : 'development',
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      publicPath: '/',
+      clean: true,
+    },
+    devServer: {
+      port: 3002,
+      historyApiFallback: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
           },
         },
-      },
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "alumnos",
+        filename: "remoteEntry.js",
+        exposes: {
+          "./App": "./src/App",
+        },
+        shared: {
+          react: { singleton: true, requiredVersion: false },
+          "react-dom": { singleton: true, requiredVersion: false },
+        },
+      }),
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      }),
     ],
-  },
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "alumnos",
-      filename: "remoteEntry.js",
-      exposes: {
-        "./App": "./src/App",
-      },
-      shared: {
-        react: { singleton: true, requiredVersion: false },
-        "react-dom": { singleton: true, requiredVersion: false },
-      },
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
+  };
 };
