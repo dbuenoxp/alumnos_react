d7387970a44f04b01f8a4d5683745e8d462885f4
Diego Alonso Bueno Pastor <dabueno@ayesa.com>
2025-09-08 19:47:02 -0500

    config prod

--- webpack.config.js
+++ webpack.config.js
@@ -2,52 +2,61 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { ModuleFederationPlugin } = require("webpack").container;
 const path = require("path");
 
-module.exports = (env, argv) => {
-  return {
-    mode: 'production',
-    entry: "./src/index.js",
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js',
-      publicPath: '/',
-      clean: true,
-    },
-    devServer: {
-      port: 3002,
-      historyApiFallback: true,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env", "@babel/preset-react"],
-            },
+module.exports = {
+  entry: "./src/index.js",
+  mode: "production",
+  output: {
+    publicPath: "auto",
+    clean: true,
+  },
+  devServer: {
+    port: 3002,
+    static: path.join(__dirname, "public"),
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      // JSX / JS
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
           },
         },
-      ],
-    },
-    resolve: {
-      extensions: [".js", ".jsx"],
-    },
-    plugins: [
-      new ModuleFederationPlugin({
-        name: "alumnos",
-        filename: "remoteEntry.js",
-        exposes: {
-          "./App": "./src/App",
+      },
+      // Imágenes (png, jpg, jpeg, webp, svg, gif)
+      {
+        test: /\.(png|jpe?g|gif|webp|svg)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "static/media/[name].[contenthash][ext]",
         },
-        shared: {
-          react: { singleton: true, requiredVersion: false },
-          "react-dom": { singleton: true, requiredVersion: false },
-        },
-      }),
-      new HtmlWebpackPlugin({
-        template: "./public/index.html",
-      }),
+      },
+      // CSS (si lo necesitas)
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
-  };
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "alumnos",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./App": "./src/App",
+      },
+      shared: {
+        react: { singleton: true, requiredVersion: false },
+        "react-dom": { singleton: true, requiredVersion: false },
+      },
+    }),
+    new HtmlWebpackPlugin({ template: "./public/index.html" }),
+  ],
 };
